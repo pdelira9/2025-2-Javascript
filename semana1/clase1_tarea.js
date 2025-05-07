2081ccc3169fe3e0d2b9963f2e403fefb8b8bdb8
Luis Gallegos <pdelira9@gmail.com>
2025-05-06 20:12:08 -0600

    day 2

--- semana1/clase1_tarea.js
+++ semana1/clase1_tarea.js
@@ -7,9 +7,15 @@ async function main() {
   const num2 = Number(await ask('Ingresa el segundo número: '));
 
   const suma = num1 + num2;
+  const resta = num2 - num1;
+  const multi = num1 * num2;
+  const divi = num1 / num2;
   // TODO: Implementar la resta, multiplicación y división
 
-  console.log('Suma:', suma);
+  console.log('Si lo sumas obtienes: ', suma);
+  console.log('Si restas el primero al segundo obtienes: ', resta);
+  console.log('Si los multiplicas obtienes: ', multi);
+  console.log('Si divides el primero entre el segundo obtienes: ', divi);
   // TODO: Mostrar el resultado de la resta, multiplicación y división
 
 }
