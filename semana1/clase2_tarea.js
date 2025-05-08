6d225fa0c4fd15d8f61fa3378475ce8220188884
Luis Gallegos <pdelira9@gmail.com>
2025-05-07 18:38:13 -0600

    Resuelvo tarea de la clase X

--- semana1/clase2_tarea.js
+++ semana1/clase2_tarea.js
@@ -10,15 +10,20 @@
 const { ask } = require('../helpers/input');
 
 async function main() {
-  const temperatura = Number(await ask("¿Cuál es la temperatura actual?"));
+  const temperatura = Number(await ask("¿Cuál es la temperatura actual? "));
 
   if (temperatura < 10) {
     console.log("Hace frío");
   }
+  else if (temperatura >= 10 && temperatura < 20) {
+    console.log('Está templado');}
+  else if(temperatura >= 20 && temperatura < 30) {
+      console.log('Hace calor');
+  } else {
+    console.log('Hace mucho calor');
+  }}
   // TODO: Implementar la lógica para temperaturas entre 10 y 20 grados
   // TODO: Implementar la lógica para temperaturas entre 20 y 30 grados
   // TODO: Implementar la lógica para temperaturas superiores a 30 grados
-
-}
 
 main();
