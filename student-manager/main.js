e1babb3be597fd6c47ec7b356888a38e2f1db611
Luis Gallegos <pdelira9@gmail.com>
2025-05-22 19:56:01 -0600

    Resuelvo tarea de la clase X

--- student-manager/main.js
+++ student-manager/main.js
@@ -3,52 +3,59 @@
 let students = [];
 
 window.onload = function () {
-  const stored = localStorage.getItem("students");
-  if (stored) {
-    students = JSON.parse(stored);
-    displayStudents();
-    updateAverage();
-  }
+    const stored = localStorage.getItem("students");
+    if (stored) {
+        students = JSON.parse(stored);
+        displayStudents();
+        updateAverage();
+    }
 }
 
 function addStudent() {
-  const name = document.getElementById("nameInput").value.trim();
-  const grade = parseFloat(document.getElementById("gradeInput").value);
+    const name = document.getElementById("nameInput").value.trim();
+    if (name === "" || name === isNaN) {
+        alert("Debes de capturar un nombre valido");
+        return;
+    }
+    const grade = parseFloat(document.getElementById("gradeInput").value);
+    if (grade > 100 || grade < 0) {
+        alert("Debes de capturar una calificación valida");
+        return;
+    }
+    const student = { name, grade, status:grade>=70?"Passed":"Failed"};
+    students.push(student);
 
-  const student = { name, grade };
-  students.push(student);
-
-  saveToLocalStorage();
-  displayStudents();
-  updateAverage();
+    saveToLocalStorage();
+    displayStudents();
+    updateAverage();
 }
 
 function displayStudents() {
-  const list = document.getElementById("studentList");
-  list.innerHTML = ``;
+    const list = document.getElementById("studentList");
+    list.innerHTML = ``;
 
-  for (let i = 0; i < students.length; i++) {
-    const li = document.createElement("li");
-    li.innerHTML = `
-      <p>${students[i].name} - ${students[i].grade} </p>
+    for (let i = 0; i < students.length; i++) {
+        const li = document.createElement("li");
+        li.innerHTML = `
+      <p>${students[i].name} - ${students[i].grade} - ${students[i].status}</p>
     `;
-    list.appendChild(li);
-  }
+        list.appendChild(li);
+    }
 }
 
 function updateAverage() {
-  if (students.length === 0) {
-    return;
-  }
-  let total = 0;
-  for (let i = 0; i < students.length; i++) {
-    total = total + students[i].grade;
-  }
-  let average = total / students.length;
+    if (students.length === 0) {
+        return;
+    }
+    let total = 0;
+    for (let i = 0; i < students.length; i++) {
+        total = total + students[i].grade;
+    }
+    let average = total / students.length;
 
-  document.getElementById("averageDisplay").textContent = `Average Grade: ${average.toFixed(2)}`;
+    document.getElementById("averageDisplay").textContent = `Average Grade: ${average.toFixed(2)}`;
 }
 
 function saveToLocalStorage() {
-  localStorage.setItem("students", JSON.stringify(students));
+    localStorage.setItem("students", JSON.stringify(students));
 }
